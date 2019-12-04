b6d7eb111e96319c95644474c1dd0afe8c04ae2c
JASPMobus <mobusj@live.com>
2019-12-03 17:49:56 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,6 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/registrations/signup' do
-
     erb :'/registrations/signup'
   end
 
@@ -25,28 +24,30 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/sessions/login' do
-
     # the line of code below render the view page in app/views/sessions/login.erb
     erb :'sessions/login'
   end
 
   post '/sessions' do
     @user = User.find_by(email: params[:email], password: params[:password])
+
     if @user
       session[:user_id] = @user.id
       redirect '/users/home'
     end
+
     redirect '/sessions/login'
   end
 
   get '/sessions/logout' do
     session.clear
+    
     redirect '/'
   end
 
   get '/users/home' do
-
     @user = User.find(session[:user_id])
+
     erb :'/users/home'
   end
 end
